403f4ff23fbbe287dcc31b9c1cba05e761b0e1dd
Andy <andyn1291@gmail.com>
2014-03-07 11:31:58 -0500

    Modified profile tempales to display dashboard for logged in users

--- users/views.py
+++ users/views.py
@@ -5,7 +5,14 @@ from django.contrib.auth.decorators import login_required
 from profiles import views as ProfileView
 
 def user_home(request):
-    if request.user.is_authenticated(): #redirect the user to there profile upon login
+    if request.user.is_authenticated(): #redirect logged in user to their profile 
         return  redirect(reverse(ProfileView.profile_detail, args=[request.user.username]))
+    else:
+        return redirect(reverse('auth_login'))
+
+# Currently unused
+def login_check(request, next=None):
+    if request.user.is_authenticated():
+        return redirect(reverse(next))
     else:
         return redirect(reverse('auth_login'))
