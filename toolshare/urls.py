9b7e566b379e91717fb9fca729e5949df7907740
Andy <andyn1291@gmail.com>
2014-03-06 21:37:55 -0500

    Added logout

--- toolshare/urls.py
+++ toolshare/urls.py
@@ -5,20 +5,20 @@ admin.autodiscover()
 from users.forms import CustomRegistrationForm, ProfileForm
 
 from registration.backends.default.views import RegistrationView
-from users import views
+from users import views as UserView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'toolshare.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', UserView.user_home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^register/$', 
        RegistrationView.as_view(form_class = CustomRegistrationForm), 
        name = 'registration_register'),
    # url(r'^profiles/edit', 'profiles.views.edit_profile', {'form_class': ProfileForm,}),
     url(r'^', include('registration.backends.default.urls')),
-    url(r'^profile/$', views.user_home, name='user_home' ),
+    url(r'^profile/$', UserView.user_home, name='user_home' ),
     url(r'^profile/', include('profiles.urls')),
 
 )
