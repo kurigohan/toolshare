bb21251b59335b04f186ef31b6c36b0cb2707a53
ajr1644 <ajr1644@rit.edu>
2014-03-07 19:16:19 -0500

    Added class ToolModel to sharecenter models.py

--- sharecenter/models.py
+++ sharecenter/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from users.models import UserProfile
+import datetime
 
 # Create your models here.
+class ToolModel(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=250)
+    shed = models.ForeignKey(
+    owner = models.ForeignKey(UserProfile)
+    borrower = models.ForeignKey(UserProfile, blank=True)
+    category = models.CharField(max_length=30)
+    dateBorrowed = models.DateTimeField(verbose_name='borrow date')
+    timeLimit = models.IntegerField(verbose_name='time limit')
+
+    @classmethod
+    def isAvailable():
+        if borrower
+            return False
+        else
+            return True
+    
+    def unicode():
+        return name
+    
+    def borrowTool(User user):
+        borrower = user
+        dateBorrowed = datetime.now
+    
+    def returnTool():
+        borrower = None
+        dateBorrowed = None
