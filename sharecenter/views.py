730f6608f1a0dc3a06da51ec4b75b5734100a191
ajr1644 <ajr1644@rit.edu>
2014-03-08 16:05:20 -0500

    Modfied (completed?) sharecenter/views.py

--- sharecenter/views.py
+++ sharecenter/views.py
@@ -7,22 +7,18 @@ def create_tool(request):
     description=request.POST['description']
     category=request.POST['category']
     owner=request.user.id
-    return #url that requested page
+    return render(request,template_name='tools/create_tool.html')
 
-def my_tools(request, template_name='my_tools.html'):
-    """
-    Calls the full list of tools and pulls only the tools with an
-    owner id that matches the user making the request.
-    """
-    return render_to_response
+def my_tools(request):
+    toolList = request.user.profile.tool_set()
+    return render(request,template_name='tools/my_tools.html',{'tool':tool,toolList})
 
-def edit_tool(request, template_name='tools/edit_tool.html'):
-    """
-    Takes form information from request, updates tool and updates
-    tool database.
-    """
-    return render
+def edit_tool(request):
+    name=request.POST['tool_name']
+    description=request.POST['description']
+    category=request.POST['category']
+    return render(request,template_name='tools/edit_tool.html')
 
 def tool_detail(request, tool_id):
     tool=get_object_or_404(ToolModel, pk=tool_id)
-    return render(tool)
+    return render(request,template_name='tools/tool_detail.html',tool)
