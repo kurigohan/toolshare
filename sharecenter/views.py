852abe46ee55c04e09464aef908490970b02ad9f
Andy <andyn1291@gmail.com>
2014-03-08 16:35:08 -0500

    Implemented create_tool view

--- sharecenter/views.py
+++ sharecenter/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def create_tool(request):
-    name=request.POST['tool_name']
-    description=request.POST['description']
-    category=request.POST['category']
-    owner=request.user.id
-    return render(request,template_name='tools/create_tool.html')
+def create_tool(request, template_name='tools/create_tool.html'):
+    if request.method == 'POST':
+        form = ToolCreationForm(data=request.POST)
+        if form.is_valid():
+            tool_obj = form.save(commit=False)
+            tool_obj.owner = request.user
+            tool_obj.save()
+            return redirect(reverse('user_home'))
+    else: 
+        form = ToolCreationForm()
 
-def my_tools(request):
-    toolList = request.user.profile.tool_set()
-    return render(request,template_name='tools/my_tools.html',{'tool':tool,toolList})
+    return render(template_name, {'form': form})
 
-def edit_tool(request):
-    name=request.POST['tool_name']
-    description=request.POST['description']
-    category=request.POST['category']
-    return render(request,template_name='tools/edit_tool.html')
+def my_tools(request, template_name='my_tools.html'):
+    """
+    Calls the full list of tools and pulls only the tools with an
+    owner id that matches the user making the request.
+    """
+    return render_to_response
+
+def edit_tool(request, template_name='tools/edit_tool.html'):
+    """
+    Takes form information from request, updates tool and updates
+    tool database.
+    """
+    return render
 
 def tool_detail(request, tool_id):
     tool=get_object_or_404(ToolModel, pk=tool_id)
-    return render(request,template_name='tools/tool_detail.html',tool)
+    return render(tool)
