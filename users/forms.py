08149705a889da11a334fc7bc1824fcdd17188da
Andy <andyn1291@gmail.com>
2014-03-17 15:19:38 -0400

    added share_count check for share_zone template and fixed borrower check in tool_detail template

--- users/forms.py
+++ users/forms.py
@@ -22,33 +22,15 @@ class AccountForm(forms.Form):
     """
     Form for editting account info.
     """
-    first_name = forms.CharField(label='First Name', max_length=30)
-    last_name = forms.CharField(label='Last Name', max_length=30)
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-    email = forms.EmailField(label='E-Mail')
+    first_name = forms.CharField(label='First Name', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label='Last Name', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='E-Mail', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    postal_code = forms.CharField(label = "Postal Code", max_length=10, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-    def clean_password(self):
-        """
-        Verifiy that the values entered into the two password fields
-        match. Note that an error here will end up in
-        ``non_field_errors()`` because it doesn't apply to a single
-        field.
-        """
-        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
-            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError("The two password fields didn't match.")
-        return self.cleaned_data
-
-    def clean_email(self):
-        """
-        Validate that the supplied email address is unique for the
-        site.
-        
-        """
-        if User.objects.filter(email__iexact=self.cleaned_data['email']):
-            raise forms.ValidationError("This email address is already in use. ")
-        return self.cleaned_data['email']
+#   def clean_email(self):
+   #     if User.objects.filter(email__iexact=self.cleaned_data['email']):
+   #         raise forms.ValidationError("This email address is already in use. ")
+   #     return self.cleaned_data['email']
 
  
 class ProfileForm(ModelForm):
--- users/views.py
+++ users/views.py
@@ -15,6 +15,27 @@ def user_home(request):
     return  redirect(reverse(ProfileView.profile_detail, args=[request.user.username]))
 user_home = login_required(user_home)
 
+@login_required
+def edit_account(request, template_name='users/edit_account.html'):
+    """
+    Change profile and personal info
+    """
+    #user = request.user
+    if request.method == 'POST':
+        form = AccountForm(data=request.POST)
+        if form.is_valid():
+            request.user.first_name == form.cleaned_data['first_name']
+            request.user.last_name == form.cleaned_data['last_name']
+            request.user.email == form.cleaned_data['email']
+            request.user.profile.postal_code == form.cleaned_data['postal_code']
+            request.user.profile.save()
+            request.user.save()
+            return  redirect(reverse(ProfileView.profile_detail, args=[request.user.username]))
+    else:
+        form = AccountForm(initial={'first_name': request.user.first_name, 'last_name':request.user.last_name, 'email': request.user.email, 'postal_code':request.user.profile.postal_code})
+    return render(request, template_name, {'form':form})
+edit_account = login_required(edit_account)
+
 class CustomRegistrationView(RegistrationView):
     """
     Override get_success_url for RegistrationView
