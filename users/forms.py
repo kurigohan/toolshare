2f170021266079cbcb4487749b05ad308cccd440
Andy <andyn1291@gmail.com>
2014-03-23 22:12:40 -0400

    Changed template styling for edit_account

--- users/forms.py
+++ users/forms.py
@@ -45,6 +45,6 @@ class ProfileForm(ModelForm):
 
 from django.contrib.auth.forms import PasswordChangeForm
 class CustomPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(label='Old Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    old_password = forms.CharField(label='Current Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     new_password2 = forms.CharField(label='Confirm New Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
