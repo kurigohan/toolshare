32c57d331ea640f3dbee685a935a0e27e5e28cd6
Andy <andyn1291@gmail.com>
2014-04-18 14:32:53 -0400

    Added files containing constant definitions for notification and borrow request

---
+++ notifications/NoticeType.py
@@ -0,0 +1,7 @@
+#Constants for notification notice_type field
+
+SYSTEM = 0;
+ALERT = 1;
+REQUEST = 2;
+ACTIVITY = 3
+
---
+++ sharecenter/RequestStatus.py
@@ -0,0 +1,8 @@
+#Constants for BorrowRequest status field
+
+APPROVED = 0;
+DENIED = 1;
+PENDING = 2;
+CANCELED = 3;
+
+
