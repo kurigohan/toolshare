2592f8e2c9f3fc55806788740b2b5958d3096748
Andy <andyn1291@gmail.com>
2014-04-19 23:03:50 -0400

    Moved urls for user

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from users import views as UserView
+
+urlpatterns = patterns('',
+                            url(r'^password_change', UserView.password_change, 
+                                                                                            name='password_change'),
+                            url(r'^edit_info', UserView.edit_account, name='edit_account'),
+                       )
