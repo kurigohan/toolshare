aeea30161b8737a265d8c402199add535e80b4ae
Andy <andyn1291@gmail.com>
2014-04-20 00:23:12 -0400

    added back slash at end of url

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from users import views as UserView
 
 urlpatterns = patterns('',
-                            url(r'^password_change', UserView.password_change, 
+                            url(r'^password_change/', UserView.password_change, 
                                                                                             name='password_change'),
-                            url(r'^edit_info', UserView.edit_account, name='edit_account'),
+                            url(r'^edit_info/', UserView.edit_account, name='edit_account'),
                        )
